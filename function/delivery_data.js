73d199a5c20a892cfa6ff9fb16c1a2eeeaf18114
tc0001 <cmtong123@gmail.com>
2023-05-06 22:05:17 +0800

    Add files via upload

---
+++ function/delivery_data.js
@@ -0,0 +1,11 @@
+var custname = localStorage.getItem("nameBox");
+var phone = localStorage.getItem("phoneBox");
+var email = localStorage.getItem("emailBox");
+var region = localStorage.getItem("regionBox");
+var postcode = localStorage.getItem("postcodeBox");
+var detail = localStorage.getItem("detailBox");
+var home = localStorage.getItem("home");
+var work = localStorage.getItem("work");
+
+document.getElementById("name").textContent = custname;
+document.getAnimations("email").textContent = email;
